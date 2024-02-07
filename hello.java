3e6dee44024126482c67beb132ed7fa093459b87
likispt <159224318+likispt@users.noreply.github.com>
2024-02-07 10:25:32 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ public class hello
 	public static void main(String[] args)
 	{
 	  System.out.println("welcome to spt");
-	  System.out.println("tumkur mysore");
+	  System.out.println("tumkur mysore banglore");
 	}
 }
